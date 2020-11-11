75890cbb9ec74e7888f4cad71a01eee474ef8f50
Vinicius Feitosa da Silva <viniciusfesil@gmail.com>
2020-11-11 11:09:04 -0300

    ⚡️ Online / Offline discord notification

--- .gitlab/notification/start.sh
+++ .gitlab/notification/start.sh
@@ -4,23 +4,22 @@ scriptFolder=$(dirname $0)
 configs="${scriptFolder}/config.sh"
 source $configs
 
-
-
 containers=`docker ps -f status=running --format "{{.Names}}"`
 for container in $listOfContainers
 do
 
-  if echo $containers |grep -q $container then  
+  if echo $containers |grep -q $container 
+  then 
     echo "$container online "
+    message="Alright! The container $container is up ! \n \n"
   else 
-    message="Warning! Bad news the container $container is down ! \n \n"
-
-    curl -H "Content-Type: application/json" \
-    -X POST \
-    -d "{\"username\": \"${username}\", \"content\": \"${message}\"}" $url
-    
     echo "$container offline"
+    message="Warning! The container $container is down ! \n \n"
+    
     #exit 1
   fi
+  curl -H "Content-Type: application/json" \
+  -X POST \
+  -d "{\"username\": \"${username}\", \"content\": \"${message}\"}" $url
 done
 exit 0
